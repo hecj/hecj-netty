049a8ab162792e66c8db04c521f63e91f2ef5354
何超杰 <hechaojie@xylink.cn>
2020-06-29 17:03:58 +0800

    拆包粘包问题

--- src/main/java/cn/hecj/netty/Decoder/Client/TimeClientHandler.java
+++ src/main/java/cn/hecj/netty/Decoder/Client/TimeClientHandler.java
@@ -1,18 +1,14 @@
-package cn.hecj.netty.netty.Client;
+package cn.hecj.netty.Decoder.Client;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+
 import java.util.logging.Logger;
 
 /**
- * Created by Idea 14 whih "netty"
- *
- * @Auhor: karl.zhao
- * @Email: karl.zhao@qq.com
- * @Date: 2015-11-28
- * @Time: 16:58
+ * by hecj
  */
 public class TimeClientHandler extends ChannelHandlerAdapter{
 
@@ -20,22 +16,17 @@ public class TimeClientHandler extends ChannelHandlerAdapter{
     private static final Logger logger =
             Logger.getLogger(TimeClientHandler.class.getName());
 
-    private final ByteBuf firstMessage;
-
-    public TimeClientHandler(){
-        byte[] req = "查询服务端时间".getBytes();
-        firstMessage = Unpooled.buffer(req.length);
-        firstMessage.writeBytes(req);
-    }
+    private int counter;
+    public TimeClientHandler(){}
 
     @Override
     public void channelRead(ChannelHandlerContext ctx,Object msg)
             throws Exception{
         ByteBuf buf = (ByteBuf)msg;
-        byte[]req = new byte[buf.readableBytes()];
+        byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
         String body = new String(req,"UTF-8");
-        System.out.println("收到服务端消息 : " + body);
+        System.out.println("Now is : " + body+";the countor is : "+ (++counter));
     }
 
     @Override
@@ -44,8 +35,13 @@ public class TimeClientHandler extends ChannelHandlerAdapter{
         // 发送查询时间的指令给服务端。
         // 调用ChannelHandlerContext的writeAndFlush方法，将请求消息发送给服务端
         // 当服务端应答时，channelRead方法被调用
-        ctx.writeAndFlush(firstMessage);
-//        ctx.writeAndFlush("我再发送个消息");
+        ByteBuf message = null;
+        for (int i=0;i<100;i++){
+            byte[] req = ("我要查询服务器时间"+i).getBytes();
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
     }
 
     @Override
