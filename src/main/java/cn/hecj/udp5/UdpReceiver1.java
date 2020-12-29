76c4e553e2e73bfb631dc2ce09ca2323326d8828
何超杰 <hechaojie@xylink.cn>
2020-12-29 10:24:03 +0800

    udp发送

--- src/main/java/cn/hecj/udp5/UdpReceiver1.java
+++ src/main/java/cn/hecj/udp5/UdpReceiver1.java
@@ -7,6 +7,8 @@ import java.net.SocketAddress;
 
 public class UdpReceiver1 {
 
+    static SocketAddress sendAddress;
+
     public static void main(String[] args) throws Exception{
 
 //        InetAddress address = InetAddress.getByName("127.0.0.1");
@@ -15,29 +17,57 @@ public class UdpReceiver1 {
 //        DatagramSocket socket = new DatagramSocket(port, address);
         DatagramSocket socket = new DatagramSocket(port);
 
-        byte[] buf = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
+        // 接收
+        receive(socket);
 
-        String getMsg = new String(buf, 0, packet.getLength());
-        System.out.println("客户端发送的数据为:"+getMsg);
+        // 发送
+        send(socket);
 
-        InetAddress clientAddress = packet.getAddress();
-        int clientPort = packet.getPort();
-        SocketAddress sendAddress = packet.getSocketAddress();
-
-        send(socket, sendAddress);
+        Thread.sleep(10000000l);
 
         socket.close();
     }
 
-    public static void send(DatagramSocket socket,SocketAddress sendAddress)throws Exception{
-        for(int i=0;i<10;i++){
+
+    public static void receive(DatagramSocket socket ){
+        new Thread(){
+            @Override
+            public void run() {
+                try{
+                    while(true){
+                        byte[] buf = new byte[1024];
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                        socket.receive(packet);
+
+                        String getMsg = new String(buf, 0, packet.getLength());
+                        System.out.println("客户端发送的数据为:"+getMsg);
+
+                        InetAddress clientAddress = packet.getAddress();
+                        int clientPort = packet.getPort();
+                        sendAddress = packet.getSocketAddress();
+                    }
+                } catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    public static void send(DatagramSocket socket)throws Exception{
+        for(int i=0;i<1000;i++){
+            if(sendAddress == null){
+                System.out.println("sendAddress为空");
+                Thread.sleep(1000l);
+                continue;
+            }
+            Thread.sleep(1000l);
+
             String feedback = "Received"+i;
             byte[] backbuf = feedback.getBytes();
   //        DatagramPacket sendPacket = new DatagramPacket(backbuf, backbuf.length,clientAddress,clientPort);
             DatagramPacket sendPacket = new DatagramPacket(backbuf, backbuf.length,sendAddress);
-            System.out.println("发送数据开始:");
+            System.out.println("发送数据开始:"+sendAddress);
             socket.send(sendPacket);
             System.out.println("发送数据完成");
         }
